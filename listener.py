3ca63f8d96088b2c6801082530d1329b8ba8040b
Andrew McKnight <thedonk@gmail.com>
2022-10-06 14:07:59 -0700

    Initial commit

---
+++ listener.py
@@ -0,0 +1,30 @@
+import keyboard
+import pygsheets
+
+
+class Listener():
+    attempt = 2
+    client = pygsheets.authorize(service_account_file="service-account-credentials.json")
+    sh = None
+
+    def __init__(self, title):
+        self.sh = self.client.open(title).sheet1
+
+    def recordHits(self, hits):
+        print(hits)
+        self.sh.update_value('A'+str(self.attempt), hits)
+        self.attempt+=1
+
+if __name__ == '__main__':
+    l = Listener('Hits on Bowser')
+
+    keyboard.add_hotkey('ctrl+0', lambda: l.recordHits(0))
+    keyboard.add_hotkey('ctrl+1', lambda: l.recordHits(1))
+    keyboard.add_hotkey('ctrl+2', lambda: l.recordHits(2))
+    keyboard.add_hotkey('ctrl+3', lambda: l.recordHits(3))
+    keyboard.add_hotkey('ctrl+4', lambda: l.recordHits(4))
+    keyboard.add_hotkey('ctrl+5', lambda: l.recordHits(5))
+    keyboard.add_hotkey('ctrl+6', lambda: l.recordHits(6))
+    keyboard.add_hotkey('ctrl+7', lambda: l.recordHits(7))
+
+    keyboard.wait()
